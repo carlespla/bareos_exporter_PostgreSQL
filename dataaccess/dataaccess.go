48f075317357d3b485c053b6e65d47b43199c6d4
Carles Pla <pla@hornetsecurity.com>
2024-06-12 11:27:17 +0200

    Error clean

--- dataaccess/dataaccess.go
+++ dataaccess/dataaccess.go
@@ -28,12 +28,11 @@ func GetConnection(connectionString string) (*connection, error) {
 func (connection connection) GetServerList() ([]string, error) {
 	date := fmt.Sprintf("%s%%", time.Now().Format("2006-01-02"))
 	//query := "SELECT DISTINCT Name FROM job WHERE TO_CHAR(SchedTime, 'YYYY-MM-DD') LIKE $1"
-	p:=
 	query := "SELECT DISTINCT Name FROM job WHERE TO_CHAR(SchedTime, 'YYYY-MM-DD') LIKE ?"
 	log.Info(query, date)
 	// results, err := connection.DB.Query("SELECT DISTINCT Name FROM job WHERE SchedTime LIKE ?", date)
 	log.Printf("SQL Query: %s, Param: %s", query, date)
-	p := "2024-06-%" 
+	p := "2024-06-%"
 	results, err := connection.DB.Query(query, p)
 	log.Printf("SQL Query: %s, Param: %s, Result: %s, Errors: %s", query, date, results, err)
 
