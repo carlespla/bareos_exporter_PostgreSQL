7612c9a5b3426c1b1bd443eef6a0ff119366d1c7
Carles Pla <pla@hornetsecurity.com>
2024-06-12 11:51:28 +0200

    Add print

--- dataaccess/dataaccess.go
+++ dataaccess/dataaccess.go
@@ -31,9 +31,10 @@ func (connection connection) GetServerList() ([]string, error) {
 	query := "SELECT DISTINCT Name FROM job WHERE TO_CHAR(SchedTime, 'YYYY-MM-DD') LIKE ?"
 	log.Info(query, date)
 	// results, err := connection.DB.Query("SELECT DISTINCT Name FROM job WHERE SchedTime LIKE ?", date)
-	p := "2024-06-0%"
+	p := "2024-06-11%"
 	log.Printf("SQL Query: %s, Param: %s", query, p)
 	results, err := connection.DB.Query(query, p)
+	log.Info(results, err)
 
 	if err != nil {
 		return nil, err
