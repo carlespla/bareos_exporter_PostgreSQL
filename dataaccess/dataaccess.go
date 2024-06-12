108dd6607f6943846e3e27c861a5a6f33d8da5f6
Carles Pla <pla@hornetsecurity.com>
2024-06-12 09:31:00 +0200

    Add query var

--- dataaccess/dataaccess.go
+++ dataaccess/dataaccess.go
@@ -27,10 +27,10 @@ func GetConnection(connectionString string) (*connection, error) {
 // GetServerList reads all servers with scheduled backups for current date
 func (connection connection) GetServerList() ([]string, error) {
 	date := fmt.Sprintf("%s%%", time.Now().Format("2006-01-02"))
-	log.Info("SELECT DISTINCT Name FROM job WHERE SchedTime LIKE ?", date)
-	log.Info("SELECT DISTINCT Name FROM job WHERE TO_CHAR(SchedTime, 'YYYY-MM-DD') LIKE ", date)
+	query := "SELECT DISTINCT Name FROM job WHERE TO_CHAR(SchedTime, 'YYYY-MM-DD') LIKE ?"
+	log.Info(query, date)
 	// results, err := connection.DB.Query("SELECT DISTINCT Name FROM job WHERE SchedTime LIKE ?", date)
-	results, err := connection.DB.Query("SELECT DISTINCT Name FROM job WHERE TO_CHAR(SchedTime, 'YYYY-MM-DD') LIKE ", date)
+	results, err := connection.DB.Query(query, date)
 
 	if err != nil {
 		return nil, err
@@ -49,7 +49,7 @@ func (connection connection) GetServerList() ([]string, error) {
 
 // TotalBytes returns total bytes saved for a server since the very first backup
 func (connection connection) TotalBytes(server string) (*types.TotalBytes, error) {
-	results, err := connection.DB.Query("SELECT SUM(JobBytes) FROM job WHERE Name=?", server)
+	results, err := connection.DB.Query("SELECT SUM(JobBytes) FROM job WHERE Name=$1", server)
 
 	if err != nil {
 		return nil, err
@@ -66,7 +66,7 @@ func (connection connection) TotalBytes(server string) (*types.TotalBytes, error
 
 // TotalFiles returns total files saved for a server since the very first backup
 func (connection connection) TotalFiles(server string) (*types.TotalFiles, error) {
-	results, err := connection.DB.Query("SELECT SUM(JobFiles) FROM job WHERE Name=?", server)
+	results, err := connection.DB.Query("SELECT SUM(JobFiles) FROM job WHERE Name=$1", server)
 
 	if err != nil {
 		return nil, err
@@ -83,7 +83,7 @@ func (connection connection) TotalFiles(server string) (*types.TotalFiles, error
 
 // LastJob returns metrics for latest executed server backup
 func (connection connection) LastJob(server string) (*types.LastJob, error) {
-	results, err := connection.DB.Query("SELECT Level,JobBytes,JobFiles,JobErrors,DATE(StartTime) AS JobDate FROM job WHERE Name LIKE ? ORDER BY StartTime DESC LIMIT 1", server)
+	results, err := connection.DB.Query("SELECT Level,JobBytes,JobFiles,JobErrors,DATE(StartTime) AS JobDate FROM job WHERE Name LIKE $1 ORDER BY StartTime DESC LIMIT 1", server)
 
 	if err != nil {
 		return nil, err
@@ -100,7 +100,7 @@ func (connection connection) LastJob(server string) (*types.LastJob, error) {
 
 // LastJob returns metrics for latest executed server backup with Level F
 func (connection connection) LastFullJob(server string) (*types.LastJob, error) {
-	results, err := connection.DB.Query("SELECT Level,JobBytes,JobFiles,JobErrors,DATE(StartTime) AS JobDate FROM job WHERE Name LIKE ? AND Level = 'F' ORDER BY StartTime DESC LIMIT 1", server)
+	results, err := connection.DB.Query("SELECT Level,JobBytes,JobFiles,JobErrors,DATE(StartTime) AS JobDate FROM job WHERE Name LIKE $1 AND Level = 'F' ORDER BY StartTime DESC LIMIT 1", server)
 
 	if err != nil {
 		return nil, err
@@ -118,7 +118,7 @@ func (connection connection) LastFullJob(server string) (*types.LastJob, error) 
 // ScheduledTime returns amount of scheduled jobs
 func (connection connection) ScheduledJobs(server string) (*types.ScheduledJob, error) {
 	date := fmt.Sprintf("%s%%", time.Now().Format("2006-01-02"))
-	results, err := connection.DB.Query("SELECT COUNT(DATE(SchedTime)) AS JobsScheduled FROM job WHERE Name LIKE ? AND SchedTime >= ?", server, date)
+	results, err := connection.DB.Query("SELECT COUNT(DATE(SchedTime)) AS JobsScheduled FROM job WHERE Name LIKE $1 AND SchedTime >= $2", server, date)
 
 	if err != nil {
 		return nil, err
